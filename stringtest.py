c367ed12ded124ae2d1a436bced12d6ad15180bb
tking8 <54691809+tking8@users.noreply.github.com>
2020-09-27 20:38:15 -0700

    Add files via upload

---
+++ stringtest.py
@@ -0,0 +1,9 @@
+string="abcdefghi"
+alist=list(string)
+print(string)
+print(alist)
+alist.reverse() #reverse order of alist
+newstring="".join(alist)
+print(newstring)
+newstring2="-".join(alist) #Join with linking character
+print(newstring2)
